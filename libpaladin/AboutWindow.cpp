48b65ae6546c015d894077aa57627cfce85cc88f
DarkWyrm <darkwyrm@gmail.com>
2010-08-10 23:59:38 +0000

    Some significant cleanups to move AboutWindow into a proper control
    HG:
    HG: All lines starting with HG: will be ignored.
    HG: ----------------------------------------------------
    HG: M libpaladin/AboutWindow.cpp
    HG: M libpaladin/AboutWindow.h
    HG:

--- libpaladin/AboutWindow.cpp
+++ libpaladin/AboutWindow.cpp
@@ -1,33 +1,54 @@
+#include "AboutWindow.h"
+
 #include <Application.h>
 #include <AppFileInfo.h>
+#include <Bitmap.h>
+#include <Font.h>
 #include <Roster.h>
 #include <String.h>
 #include <stdio.h>
-#include <Screen.h>
 #include <TranslationUtils.h>
-#include "AboutWindow.h"
+#include <View.h>
 
-AboutWindow::AboutWindow(void)
- : BWindow(BRect(100,100,500,400),"", B_MODAL_WINDOW_LOOK,
- 	B_MODAL_APP_WINDOW_FEEL,
- 	B_NOT_ZOOMABLE | B_NOT_RESIZABLE | B_WILL_ACCEPT_FIRST_CLICK)
+class AboutView : public BView
 {
-	BScreen screen;
-	BRect screenrect(screen.Frame());
+public:
+			AboutView(BRect frame, const char *imageName,
+					const int32 &imageType);
+			~AboutView(void);
+	void	AttachedToWindow(void);
+	void	Draw(BRect update);
+	void	MouseDown(BPoint pt);
+	void	KeyUp(const char *bytes, int32 numbytes);
 	
-	AboutView *aboutview = new AboutView(Bounds());
+	BBitmap	*fLogo;
+	
+	char	version[256];
+	BPoint	versionpos;
+	
+	int32	fEntryCount;
+};
+
+
+AboutWindow::AboutWindow(const char *imageName, const int32 &imageType)
+	:	DWindow(BRect(100,100,500,400), "", B_MODAL_WINDOW,
+			 	B_NOT_ZOOMABLE | B_NOT_RESIZABLE | B_WILL_ACCEPT_FIRST_CLICK)
+{
+	MakeCenteredOnShow(true);
+	
+	AboutView *aboutview = new AboutView(Bounds(), imageName, imageType);
 	AddChild(aboutview);
-	aboutview->MakeFocus(true);
 	
-	MoveTo( (screenrect.Width()-Frame().Width())/2, (screenrect.Height()-Frame().Height())/2 );
+	aboutview->MakeFocus(true);
 }
 
-AboutView::AboutView(BRect frame)
+
+AboutView::AboutView(BRect frame, const char *imageName, const int32 &imageType)
  : BView (frame, "AboutView", B_FOLLOW_ALL, B_WILL_DRAW)
 {
 	SetViewColor(126,126,190);
 	
-	fLogo = BTranslationUtils::GetBitmap('JPG ',"AboutPaladin.jpg");
+	fLogo = BTranslationUtils::GetBitmap(imageType, imageName);
 	
 	app_info ai;
 	version_info vi;
@@ -68,35 +89,47 @@ AboutView::AboutView(BRect frame)
 	font_height height;
 	be_plain_font->GetHeight(&height);
 	
-	versionpos.y = fLogo->Bounds().bottom - 5;
-	versionpos.x = fLogo->Bounds().right - StringWidth(version) - 7;
+	BRect r(fLogo ? fLogo->Bounds() : Bounds());
+	versionpos.y = r.bottom - 5;
+	versionpos.x = r.right - StringWidth(version) - 7;
 	
 	SetDrawingMode(B_OP_OVER);
 	SetFont(be_bold_font);
 }
+
 
 AboutView::~AboutView(void)
 {
 	delete fLogo;
 }
 
-void AboutView::MouseDown(BPoint pt)
+
+void
+AboutView::MouseDown(BPoint pt)
 {
 	Window()->PostMessage(B_QUIT_REQUESTED);
 }
 
-void AboutView::AttachedToWindow(void)
+
+void
+AboutView::AttachedToWindow(void)
 {
-	Window()->ResizeTo(fLogo->Bounds().Width(),fLogo->Bounds().Height());
+	if (fLogo)
+		Window()->ResizeTo(fLogo->Bounds().Width(),fLogo->Bounds().Height());
 }
 
-void AboutView::Draw(BRect update)
+
+void
+AboutView::Draw(BRect update)
 {
-	DrawBitmap(fLogo, BPoint(0,0));
+	if (fLogo)
+		DrawBitmap(fLogo, BPoint(0,0));
 	DrawString(version,versionpos);
 }
 
-void AboutView::KeyUp(const char *bytes, int32 numbytes)
+
+void
+AboutView::KeyUp(const char *bytes, int32 numbytes)
 {
 	Window()->PostMessage(B_QUIT_REQUESTED);
 }

76c0cc582ed13a1a5561dc2640fd3645ea02d492
DarkWyrm <darkwyrm@gmail.com>
2010-08-14 14:37:41 +0000

    Updated the PostInstall script for PkgBuilder packages

--- Paladin/PostInstall.sh
+++ Paladin/PostInstall.sh
@@ -5,20 +5,27 @@ case $OSTYPE in
 	mkdir /boot/home/config/ZetaMenu/Software/Development/Paladin/
 	ln -s /boot/apps/Paladin/Paladin /boot/home/config/ZetaMenu/Software/Development/Paladin/
 	ln -s /boot/apps/Paladin/PalEdit /boot/home/config/ZetaMenu/Software/Development/Paladin/
-	ln -s /boot/apps/Paladin/Paladin\ 1.2\ Documentation.pdf /boot/home/config/ZetaMenu/Software/Development/Paladin/
+	ln -s /boot/apps/Paladin/README /boot/home/config/ZetaMenu/Software/Development/Paladin/
+	ln -s /boot/apps/Paladin/WHAT\'S\ NEW /boot/home/config/ZetaMenu/Software/Development/Paladin/
+	ln -s /boot/apps/Paladin/Paladin\ Documentation.pdf /boot/home/config/ZetaMenu/Software/Development/Paladin/
 	mv -f ccache.zeta /boot/home/config/bin/ccache
 	;;
 "Haiku")
 	mkdir /boot/home/config/be/Applications/Paladin/
 	ln -s /boot/apps/Paladin/Paladin /boot/home/config/be/Applications/Paladin/
 	ln -s /boot/apps/Paladin/PalEdit /boot/home/config/be/Applications/Paladin/
-	ln -s /boot/apps/Paladin/Paladin\ 1.2\ Documentation.pdf /boot/home/config/be/Applications/Paladin/
-	mv -f ccache.haiku /boot/home/config/bin/ccache
+	ln -s /boot/apps/Paladin/README /boot/home/config/be/Applications/Paladin/
+	ln -s /boot/apps/Paladin/WHAT\'S\ NEW /boot/home/config/be/Applications/Paladin/
+	ln -s /boot/apps/Paladin/Paladin\ Documentation.pdf /boot/home/config/be/Applications/Paladin/
+	mv -f ccache /boot/home/config/bin/ccache
+	mv -f fastdep /boot/home/config/bin/ccache
 	;;
 *)
 	mkdir /boot/home/config/be/Applications/Paladin/
 	ln -s /boot/apps/Paladin/Paladin /boot/home/config/be/Applications/Paladin/
 	ln -s /boot/apps/Paladin/PalEdit /boot/home/config/be/Applications/Paladin/
-	ln -s /boot/apps/Paladin/Paladin\ 1.2\ Documentation.pdf /boot/home/config/be/Applications/Paladin/
+	ln -s /boot/apps/Paladin/README /boot/home/config/be/Applications/Paladin/
+	ln -s /boot/apps/Paladin/WHAT\'S\ NEW /boot/home/config/be/Applications/Paladin/
+	ln -s /boot/apps/Paladin/Paladin\ Documentation.pdf /boot/home/config/be/Applications/Paladin/
 	;;
 esac

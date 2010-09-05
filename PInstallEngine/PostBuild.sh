626a0bc58579bbff88bcb39d2a9a450a236887d3
DarkWyrm <darkwyrm@gmail.com>
2010-09-05 14:45:03 +0000

    Made a separate folder for tests and remove some more unnecessary files
    --HG--
    rename : CalcPkg.sh => tests/CalcPkg.sh
    rename : PSfx/TestCLI.sh => tests/TestCLI.sh

--- PInstallEngine/PostBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-cp PInstallEngine PInstallEngineTest.sfx
-rc PInstallEngineTest.rdef -o PInstallEngineTest.rsrc
-xres -o PInstallEngineTest.sfx PInstallEngineTest.rsrc
-rm PInstallEngineTest.rsrc
-cat testdata/calc/files.zip >> PInstallEngineTest.sfx
-chmod +x PInstallEngineTest.sfx

82367c86f70477724982089326714a20b7dc839e
DarkWyrm <darkwyrm@gmail.com>
2010-12-22 20:12:42 +0000

    Mostly implemented PSlider class
    HG:
    HG: All lines starting with HG: will be ignored.
    HG: ----------------------------------------------------
    HG: M PDesigner/NOTES
    HG: M PDesigner/PDesigner.pld
    HG: A PDesigner/PSlider.cpp
    HG: A PDesigner/PSlider.h
    HG: ? PDesigner/PDesigner
    HG:

---
+++ PDesigner/PSlider.h
@@ -0,0 +1,51 @@
+#ifndef PBUTTON_H
+#define PBUTTON_H
+
+#include <Slider.h>
+#include "PControl.h"
+
+/*
+	PSlider Properties:
+		All PView Properties
+		All PControl Properties
+		
+		BarColor
+		BarThickness
+		FillColor
+		HashMarkCount
+		HashMarkLocation
+		MinLimit
+		MinLimitLabel
+		MaxLimit
+		MaxLimitLabel
+		Orientation
+		Position
+		ThumbStyle
+		UsingFillColor
+*/
+
+class PSlider : public PControl
+{
+public:
+							PSlider(void);
+							PSlider(BMessage *msg);
+							PSlider(const char *name);
+							PSlider(const PSlider &from);
+							~PSlider(void);
+			
+	static	BArchivable *	Instantiate(BMessage *data);
+	
+	static	PObject *		Create(void);
+	virtual	PObject *		Duplicate(void) const;
+	
+	virtual	status_t		GetProperty(const char *name, PValue *value, const int32 &index = 0) const;
+	virtual	status_t		SetProperty(const char *name, PValue *value, const int32 &index = 0);
+	
+protected:
+	virtual void			InitBackend(BView *view = NULL);
+
+private:
+	void					InitProperties(void);
+};
+
+#endif

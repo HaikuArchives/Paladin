0d17c35896af580a7bd06db54a87b3af56a58a34
DarkWyrm <darkwyrm@gmail.com>
2011-02-26 18:11:28 +0000

    Initial checkin of Lua scripts needed for Code Lookup feature

---
+++ Paladin/Lua/test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+clear
+rm test.db
+find /boot/develop/headers/os -name "*.h" -exec ./IndexHaikuHeader.lua test.db '{}' \;
+echo "------------------------"
+echo "Files"
+echo "------------------------"
+sqlite3 test.db "select * from Files;"
+echo "------------------------"
+echo "Classes"
+echo "------------------------"
+sqlite3 test.db "select * from Classes;"
+echo "------------------------"
+echo "Class Entries"
+echo "------------------------"
+sqlite3 test.db "select * from ClassEntries;"
+echo "------------------------"
+echo "Defines"
+echo "------------------------"
+sqlite3 test.db "select * from Defines;"
+echo "------------------------"
+echo "Enums"
+echo "------------------------"
+sqlite3 test.db "select * from Enums;"
+

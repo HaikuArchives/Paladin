e4ed7565e543290d7de71d0c7a19946732b7c59c
DarkWyrm <darkwyrm@gmail.com>
2011-04-08 23:07:11 +0000

    Updated makefile

--- libcharlemagne/PObGen/make.sh
+++ libcharlemagne/PObGen/make.sh
@@ -3,6 +3,7 @@ lua PObGen.lua PButtonGen.lua
 lua PObGen.lua PSliderGen.lua
 lua PObGen.lua PTextViewGen.lua
 lua PObGen.lua PBoxGen.lua
+lua PObGen.lua PClipboardGen.lua
 
 mv -f *.cpp ..
 mv -f *.h ..

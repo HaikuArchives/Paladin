7632ecab60d197e7b5164328297c3424b60d8140
DarkWyrm <darkwyrm@gmail.com>
2011-04-10 18:29:20 +0000

    Add PTextControlGen.lua to the build script

--- libcharlemagne/PObGen/make.sh
+++ libcharlemagne/PObGen/make.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-lua PObGen.lua PButtonGen.lua
-lua PObGen.lua PSliderGen.lua
-lua PObGen.lua PTextViewGen.lua
 lua PObGen.lua PBoxGen.lua
+lua PObGen.lua PButtonGen.lua
 lua PObGen.lua PClipboardGen.lua
+lua PObGen.lua PSliderGen.lua
+lua PObGen.lua PTextControlGen.lua
+lua PObGen.lua PTextViewGen.lua
 
 mv -f *.cpp ..
 mv -f *.h ..

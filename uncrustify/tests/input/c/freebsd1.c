4378a7089038f28d4f5f613a00d3684a33468a9e
DarkWyrm <darkwyrm@gmail.com>
2011-04-21 07:26:35 +0000

    Add uncrustify 0.57 for a future code restyling feature.

---
+++ uncrustify/tests/input/c/freebsd1.c
@@ -0,0 +1,11 @@
+void foo (void) {
+	     int a1, a2;     /* Declare ints, too, don't default them. */
+	     float fl;	     /* Beware double vs. float prototype differences. */
+	     int a4;	     /* List in order declared.
+                              * seconds line
+                              */
+             if (foo)
+             bar();
+
+}
+

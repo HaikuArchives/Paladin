6c267ca9d3188142669a28af6ee825f5fcc4d546
DarkWyrm <darkwyrm@gmail.com>
2011-05-01 20:16:13 +0000

    Implemented PBitmapBackend and property-related methods
    HG:
    HG: All lines starting with HG: will be ignored.
    HG: ----------------------------------------------------
    HG: M libcharlemagne/PBitmap.cpp
    HG: M libcharlemagne/testlib.pld
    HG:

--- libcharlemagne/PBitmap.cpp
+++ libcharlemagne/PBitmap.cpp
@@ -22,10 +22,12 @@ public:
 	color_space	ColorSpace(void) const;
 	BRect		Bounds(void) const;
 	
-	status_t	SetDrawingFlags(uint32 flags);
 	uint32		Flags(void);
 	
-	void		Set(const BRect &frame);
+	void		Initialize(const BRect &bounds, uint32 flags,
+							color_space space, int32 bytesPerRow,
+							screen_id screenID);
+	void		Initialize(const BBitmap &source, uint32 flags);
 	
 	void		SetBits(const void *data, int32 length,
 						int32 offset, color_space colorSpace);
@@ -137,16 +139,22 @@ PBitmap::GetProperty(const char *name, PValue *value, const int32 &index) const
 		return B_NAME_NOT_FOUND;
 	
 	PBitmapBackend *backend = fBackend;
-	if (str.ICompare("IsValid") == 0)
-		((BoolProperty*)prop)->SetValue(backend->IsValid());
+	if (str.ICompare("AreaID") == 0)
+		((IntProperty*)prop)->SetValue(backend->Area());
+	else if (str.ICompare("BytesPerRow") == 0)
+		((IntProperty*)prop)->SetValue(backend->BytesPerRow());
 	else if (str.ICompare("ColorSpace") == 0)
-		((IntProperty*)prop)->SetValue(backend->ColorSpace());
-	else if (str.ICompare("Bounds") == 0)
+		((IntProperty*)prop)->SetValue((int64)backend->ColorSpace());
+	else if (str.ICompare("Flags") == 0)
+		((IntProperty*)prop)->SetValue(backend->Flags());
+	else if (str.ICompare("Frame") == 0)
 		((RectProperty*)prop)->SetValue(backend->Bounds());
+	else if (str.ICompare("IsValid") == 0)
+		((BoolProperty*)prop)->SetValue(backend->IsValid());
+	else if (str.ICompare("Locked") == 0)
+		((BoolProperty*)prop)->SetValue(backend->IsLocked());
 	else
-	{
 		return PObject::GetProperty(name, value, index);
-	}
 
 	return prop->GetValue(value);
 }
@@ -166,7 +174,7 @@ PBitmap::SetProperty(const char *name, PValue *value, const int32 &index)
 	if (FlagsForProperty(prop) & PROPERTY_READ_ONLY)
 		return B_READ_ONLY;
 	
-	BScreen *backend = (BScreen*)fBackend;
+	PBitmapBackend *backend = fBackend;
 	
 	BoolValue boolval;
 	ColorValue colorval;
@@ -180,6 +188,17 @@ PBitmap::SetProperty(const char *name, PValue *value, const int32 &index)
 	if (status != B_OK)
 		return status;
 
+	if (str.ICompare("Locked") == 0)
+	{
+		prop->GetValue(&boolval);
+		if (*boolval.value)
+			backend->Lock();
+		else
+			backend->Unlock();
+	}
+	else
+		return PObject::SetProperty(name, value, index);
+	
 	return PObject::SetProperty(name, value, index);
 }
 
@@ -196,8 +215,6 @@ PBitmap::InitBackend(void)
 {
 	if (!fBackend)
 		fBackend = new PBitmapBackend(this);
-	StringValue sv("A representation of the screen");
-	SetProperty("Description", &sv);
 }
 
 
@@ -205,10 +222,23 @@ void
 PBitmap::InitProperties(void)
 {
 	SetStringProperty("Description", "A representation of the screen");
-
-	AddProperty(new BoolProperty("IsValid", true, "Is the screen object valid?"));
-	AddProperty(new IntProperty("ColorSpace", B_RGB_32_BIT, "Color space of the screen"));
-	AddProperty(new RectProperty("Frame", BRect(0,0,639, 479), "Size of the screen in pixels"));
+	
+	AddProperty(new IntProperty("AreaID", -1, "The area containing the bitmap's data."),
+				PROPERTY_READ_ONLY);
+	AddProperty(new IntProperty("BytesPerRow", -1,
+								"The number of bytes in one of the bitmap's rows."),
+				PROPERTY_READ_ONLY);
+	AddProperty(new IntProperty("ColorSpace", B_RGB_32_BIT,
+								"Color space of the bitmap"),
+				PROPERTY_READ_ONLY);
+	AddProperty(new IntProperty("Flags", 0, "Behavior flags for the bitmap"),
+				PROPERTY_READ_ONLY);
+	AddProperty(new RectProperty("Frame", BRect(0,0,639,479),
+								"Size of the bitmap in pixels"),
+				PROPERTY_READ_ONLY);
+	AddProperty(new BoolProperty("IsValid", true, "Is the bitmap object valid?"),
+				PROPERTY_READ_ONLY);
+	AddProperty(new BoolProperty("Locked", false, "The lock state of the bitmap."));
 }
 
 
@@ -221,7 +251,8 @@ PBitmap::InitMethods(void)
 
 
 PBitmapBackend::PBitmapBackend(PObject *owner)
-	:	fOwner(owner)
+	:	fOwner(owner),
+		fBitmap(NULL)
 {
 }
 
@@ -229,66 +260,82 @@ PBitmapBackend::PBitmapBackend(PObject *owner)
 status_t
 PBitmapBackend::InitCheck(void) const
 {
+	return fBitmap ? fBitmap->InitCheck() : B_NO_INIT;
 }
 
 
 bool
 PBitmapBackend::IsValid(void) const
 {
+	return fBitmap ? fBitmap->IsValid() : false;
 }
 
 
 area_id
 PBitmapBackend::Area(void) const
 {
+	return fBitmap ? fBitmap->Area() : -1;
 }
 
 
 void *
 PBitmapBackend::Bits(void) const
 {
+	return fBitmap ? fBitmap->Bits() : NULL;
 }
 
 
 int32
 PBitmapBackend::BitsLength(void) const
 {
+	return fBitmap ? fBitmap-> BitsLength() : 0;
 }
 
 
 int32
 PBitmapBackend::BytesPerRow(void) const
 {
+	return fBitmap ? fBitmap->BytesPerRow() : 0;
 }
 
 
 color_space
 PBitmapBackend::ColorSpace(void) const
 {
+	return fBitmap ? fBitmap->ColorSpace() : B_NO_COLOR_SPACE;
 }
 
 
 BRect
 PBitmapBackend::Bounds(void) const
 {
-}
-
-
-status_t
-PBitmapBackend::SetDrawingFlags(uint32 flags)
-{
+	return fBitmap ? fBitmap->Bounds() : BRect(0.0, 0.0, -1.0, -1.0);
 }
 
 
 uint32
 PBitmapBackend::Flags(void)
 {
+	return fBitmap ? fBitmap->Flags() : 0;
 }
 
 
 void
-PBitmapBackend::Set(const BRect &frame)
+PBitmapBackend::Initialize(const BRect &bounds, uint32 flags,
+							color_space space, int32 bytesPerRow,
+							screen_id screenID)
 {
+	if (!fBitmap)
+		fBitmap = new BBitmap(bounds, flags, space, bytesPerRow,
+								screenID);
+}
+
+
+void
+PBitmapBackend::Initialize(const BBitmap &source, uint32 flags)
+{
+	if (!fBitmap)
+		fBitmap = new BBitmap(source, flags);
 }
 
 
@@ -296,59 +343,72 @@ void
 PBitmapBackend::SetBits(const void *data, int32 length,
 					int32 offset, color_space colorSpace)
 {
+	if (fBitmap)
+		fBitmap->SetBits(data, length, offset, colorSpace);
 }
 
 
 void
 PBitmapBackend::AddChild(BView *view)
 {
+	if (fBitmap)
+		fBitmap->AddChild(view);
 }
 
 
 bool
 PBitmapBackend::RemoveChild(BView *view)
 {
+	return fBitmap ? fBitmap->RemoveChild(view) : false;
 }
 
 
 int32
 PBitmapBackend::CountChildren(void) const
 {
+	return fBitmap ? fBitmap->CountChildren() : -1;
 }
 
 
 BView *
 PBitmapBackend::ChildAt(int32 index) const
 {
+	return fBitmap ? fBitmap->ChildAt(index) : NULL;
 }
 
 
 BView *
 PBitmapBackend::FindView(const char *name) const
 {
+	return fBitmap ? fBitmap->FindView(name) : NULL;
 }
 
 
 BView *
 PBitmapBackend::FindView(BPoint pt) const
 {
+	return fBitmap ? fBitmap->FindView(pt) : NULL;
 }
 
 
 bool
 PBitmapBackend::Lock(void)
 {
+	return fBitmap ? fBitmap->Lock() : false;
 }
 
 
 void
 PBitmapBackend::Unlock(void)
 {
+	if (fBitmap)
+		fBitmap->Unlock();
 }
 
 
 bool
 PBitmapBackend::IsLocked(void) const
 {
+	return fBitmap ? fBitmap->IsLocked() : false;
 }
 

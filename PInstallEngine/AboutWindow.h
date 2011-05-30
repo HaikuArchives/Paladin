df6a8c7abc2dc73b1f6b72063c2b5e6996d4a593
DarkWyrm <darkwyrm@gmail.com>
2011-05-30 21:12:44 +0000

    Cruft removal

--- PInstallEngine/AboutWindow.h
+++
@@ -1,6 +0,0 @@
-#ifndef ABOUTWINDOW_H
-#define ABOUTWINDOW_H
-
-void ShowSplash(void);
-
-#endif
--- PSfx/CommandLine.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMANDLINE_H
-#define COMMANDLINE_H
-
-bool ProcessArgs(int argc, char **argv);
-void DoCommandLine(void);
-void BuildPackage(const char *pfxpath, const char *pkgpath, const char *platformName);
-
-#endif

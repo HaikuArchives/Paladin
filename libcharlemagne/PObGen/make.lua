803a2638b16d017e336a0e0a7db2f92dc3b25ba4
DarkWyrm <darkwyrm@gmail.com>
2011-07-02 12:39:06 +0000

    Move from a shell script to Lua to make more flexible builds

---
+++ libcharlemagne/PObGen/make.lua
@@ -0,0 +1,28 @@
+#!env lua
+
+gFileList =
+{
+	"PBox",
+	"PButton",
+	"PCheckBox",
+	"PClipboard",
+	"PColorControl",
+	"PLabel",
+	"PListView",
+	"PRadioButton",
+	"PSlider",
+	"PTextControl",
+	"PTextView",
+}
+
+
+for i = 1, #gFileList do
+	print("Building " .. gFileList[i])
+	local cmd = "lua PObGen.lua " .. gFileList[i] .. ".pobj"
+	if (os.execute(cmd) and arg[1] ~= "nomove") then
+		cmd = string.format("mv %s.cpp .. && mv %s.h ..", gFileList[i],
+							gFileList[i])
+		print("Moving " .. gFileList[i])
+		os.execute(cmd)
+	end
+end

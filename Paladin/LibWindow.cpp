e48250fc3c0059e3a0b8aa289fe298e81d630aa6
John Scipione <jscipione@gmail.com>
2014-06-19 11:53:31 -0400

    Style fixes to LibWindow

--- Paladin/LibWindow.cpp
+++ Paladin/LibWindow.cpp
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2001-2010 DarkWyrm <bpmagic@columbus.rr.com>
+ * Copyright 2014 John Scipione <jscipione@gmail.com>
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		DarkWyrm, bpmagic@columbus.rr.com
+ *		John Scipione, jscipione@gmail.com
+ */
+
+
 #include "LibWindow.h"
 
 #include <CheckBox.h>
@@ -13,19 +24,20 @@
 #include "Project.h"
 #include "Settings.h"
 
+
 #define B_USER_DEVELOP_DIRECTORY ((directory_which)3028)
 
 
-enum
-{
+enum {
 	M_TOGGLE_LIB = 'tglb'
 };
 
 
-LibraryWindow::LibraryWindow(BRect frame, const BMessenger &parent, Project *project)
+LibraryWindow::LibraryWindow(BRect frame, const BMessenger& parent,
+	Project* project)
 	:
-	DWindow(frame,NULL,B_TITLED_WINDOW,B_ASYNCHRONOUS_CONTROLS |
-		B_NOT_ZOOMABLE | B_NOT_MINIMIZABLE),
+	DWindow(frame, NULL, B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS
+		| B_NOT_ZOOMABLE | B_NOT_MINIMIZABLE),
 	fParent(parent),
 	fProject(project)
 {
@@ -34,28 +46,29 @@ LibraryWindow::LibraryWindow(BRect frame, const BMessenger &parent, Project *pro
 		title << project->GetName();
 		SetTitle(title.String());
 	}
-	
-	BView *top = GetBackgroundView();
-	
-	BRect r(10,10,11,11);
-	BStringView *label = new BStringView(r,"label",TR("Choose the system libraries "
-													"for your project."));
+
+	BView* top = GetBackgroundView();
+
+	BRect r(10, 10, 11, 11);
+	BStringView* label = new BStringView(r, "label",
+		TR("Choose the system libraries for your project."));
 	label->ResizeToPreferred();
 	top->AddChild(label);
-	label->SetText(TR("Scanning libraries…"));
-	
-	r = Bounds().InsetByCopy(10,10);
+	label->SetText(TR("Scanning libraries" B_UTF8_ELLIPSIS));
+
+	r = Bounds().InsetByCopy(10, 10);
 	r.top = label->Frame().top + 20;
 	r.right -= B_V_SCROLL_BAR_WIDTH;
-	fCheckList = new BView(r,"checklist",B_FOLLOW_ALL,B_WILL_DRAW);
-	
-	BScrollView *scrollView = new BScrollView("scrollView",fCheckList,
-											B_FOLLOW_ALL, 0,false,true);
+	fCheckList = new BView(r, "checklist", B_FOLLOW_ALL, B_WILL_DRAW);
+
+	BScrollView* scrollView = new BScrollView("scrollView", fCheckList,
+		B_FOLLOW_ALL, 0, false, true);
 	top->AddChild(scrollView);
-	
-	fScanThread = spawn_thread(ScanThread,"libscanthread",B_NORMAL_PRIORITY,this);
+
+	fScanThread = spawn_thread(ScanThread, "libscanthread", B_NORMAL_PRIORITY,
+		this);
 	resume_thread(fScanThread);
-	
+
 	fCheckList->MakeFocus(true);
 }
 
@@ -65,8 +78,9 @@ LibraryWindow::~LibraryWindow(void)
 	gSettings.Lock();
 	if (gSettings.ReplaceRect("libwin_frame",Frame()) != B_OK)
 		gSettings.AddRect("libwin_frame",Frame());
+
 	gSettings.Unlock();
-	
+
 	if (fScanThread > 0)
 		kill_thread(fScanThread);
 }
@@ -75,208 +89,211 @@ LibraryWindow::~LibraryWindow(void)
 bool
 LibraryWindow::QuitRequested(void)
 {
-	if (fProject)
+	if (fProject != NULL)
 		fProject->Save();
-	
+
 	fParent.SendMessage(M_LIBWIN_CLOSED);
+
 	return true;
 }
 
 
 void
-LibraryWindow::MessageReceived(BMessage *msg)
+LibraryWindow::MessageReceived(BMessage* message)
 {
-	switch (msg->what)
-	{
+	switch (message->what) {
 		case M_TOGGLE_LIB:
 		{
-			if (!fProject)
+			if (fProject == NULL)
 				break;
-			
+
 			entry_ref ref;
-			BString strpath;
-			if (msg->FindRef("ref",&ref) != B_OK ||
-				msg->FindString("path",&strpath) != B_OK)
+			BString pathString;
+			if (message->FindRef("ref", &ref) != B_OK
+				|| message->FindString("path", &pathString) != B_OK) {
 				break;
-			
-			// We're not using the ref's path to preserve symlinked folder names
-			// in the /boot/develop hierarchy. Both Haiku and Zeta do this.
-			BPath path(strpath.String());
+			}
+
+			// We're not using the ref's path to preserve symlinked folder
+			// names in the /boot/develop hierarchy.
+			// Both Haiku and Zeta do this.
+			BPath path(pathString.String());
 			path.Append(ref.name);
 			if (fProject->HasLibrary(path.Path()))
 				fProject->RemoveLibrary(path.Path());
 			else
 				fProject->AddLibrary(path.Path());
+
 			break;
 		}
+
 		default:
-			DWindow::MessageReceived(msg);
+			DWindow::MessageReceived(message);
 			break;
 	}
 }
 
 
-BView *
-LibraryWindow::AddHeader(BPoint location, const char *label)
+BView*
+LibraryWindow::AddHeader(BPoint location, const char* label)
 {
 	Lock();
-	BRect r(location.x,location.y,location.x + 1, location.y + 1);
-	BStringView *header = new BStringView(r,"header",label);
+
+	BRect r(location.x, location.y, location.x + 1, location.y + 1);
+	BStringView* header = new BStringView(r, "header", label);
 	fCheckList->AddChild(header);
 	header->SetFont(be_bold_font);
-	header->SetHighColor(0,0,255);
+	header->SetHighColor(0, 0 ,255);
 	header->ResizeToPreferred();
+
 	Unlock();
-	
+
 	return header;
 }
 
 
 BRect
-LibraryWindow::ScanFolder(BPoint location, const char *path, float *maxwidth)
+LibraryWindow::ScanFolder(BPoint location, const char* path, float* maxWidth)
 {
-	BDirectory dir(path);
-	if (dir.InitCheck() != B_OK)
-		return BRect(0,0,-1,-1);
-	
+	BDirectory directory(path);
+	if (directory.InitCheck() != B_OK)
+		return BRect(0, 0, -1, -1);
+
 	float width = 0.0;
-	dir.Rewind();
+	directory.Rewind();
 	entry_ref ref;
-	BRect r(location.x,location.y,location.x + 1,location.y + 1);
-	
-	while (dir.GetNextRef(&ref) == B_OK)
-	{
-		BString str(ref.name);
-		
-		int32 soPos = str.FindLast(".so");
-		int32 aPos = str.FindLast(".a");
-		
-		if (soPos < 0 && aPos < 0)
+	BRect r(location.x, location.y, location.x + 1, location.y + 1);
+
+	while (directory.GetNextRef(&ref) == B_OK) {
+		BString refString(ref.name);
+
+		int32 soPosition = refString.FindLast(".so");
+		int32 aPosition = refString.FindLast(".a");
+
+		if (soPosition < 0 && aPosition < 0)
 			continue;
-		
-		if (soPos >= 0 && soPos < str.CountChars() - 3)
+
+		if (soPosition >= 0 && soPosition < refString.CountChars() - 3)
 			continue;
-		
-		if (aPos >= 0 && aPos < str.CountChars() - 2)
+
+		if (aPosition >= 0 && aPosition < refString.CountChars() - 2)
 			continue;
-		
-		Lock();	
-		BMessage *msg = new BMessage(M_TOGGLE_LIB);
-		msg->AddRef("ref",&ref);
-		msg->AddString("path",path);
-		BCheckBox *box = new BCheckBox(r,ref.name,ref.name,msg);
-		box->ResizeToPreferred();
-		r = box->Frame();
-		fCheckList->AddChild(box);
-		fSystemLibs.AddItem(box);
-		
-		// Make sure that there isn't any shortcut normalization
-		BPath dirpath(path);
-		dirpath.Append(ref.name);
-		
-		if (fProject && fProject->HasLibrary(dirpath.Path()))
-			box->SetValue(B_CONTROL_ON);
+
+		Lock();
+		BMessage* message = new BMessage(M_TOGGLE_LIB);
+		message->AddRef("ref", &ref);
+		message->AddString("path", path);
+		BCheckBox* checkBox = new BCheckBox(r, ref.name, ref.name, message);
+		checkBox->ResizeToPreferred();
+		r = checkBox->Frame();
+		fCheckList->AddChild(checkBox);
+		fSystemLibs.AddItem(checkBox);
+
+		// make sure that there isn't any shortcut normalization
+		BPath directoryPath(path);
+		directoryPath.Append(ref.name);
+
+		if (fProject != NULL && fProject->HasLibrary(directoryPath.Path()))
+			checkBox->SetValue(B_CONTROL_ON);
 		else
-			box->SetValue(B_CONTROL_OFF);
+			checkBox->SetValue(B_CONTROL_OFF);
+
 		Unlock();
-		
-		
-		width = MAX(r.right,width);
-		r.OffsetBy(0,r.Height());
+
+		width = MAX(r.right, width);
+		r.OffsetBy(0, r.Height());
 	}
-	
+
 	if (r.Height() == 1)
 		r.Set(0, 0, -1, -1);
-	
-	*maxwidth = width;
+
+	*maxWidth = width;
+
 	return r;
 }
 
 
 int32
-LibraryWindow::ScanThread(void *data)
+LibraryWindow::ScanThread(void* data)
 {
-	LibraryWindow *win = (LibraryWindow *)data;
+	LibraryWindow* window = (LibraryWindow*)data;
 
 	float maxwidth;
 	BRect r(5, 5, 105, 20);
-	
-	BView *systemheader = win->AddHeader(r.LeftTop(), TR("System Libraries:"));
 
-	win->Lock();
-	r = systemheader->Frame();
-	win->Unlock();
+	BView* systemHeader = window->AddHeader(r.LeftTop(), TR("System Libraries:"));
+
+	window->Lock();
+	r = systemHeader->Frame();
+	window->Unlock();
 	maxwidth = r.right;
 
 	r.OffsetBy(0, r.Height() + 10);
 
 	DPath sysPath = GetSystemPath(B_USER_DEVELOP_DIRECTORY);
 	sysPath << "lib/x86";
-	BRect out = win->ScanFolder(r.LeftTop(), sysPath.GetFullPath(), &maxwidth);
+	BRect out = window->ScanFolder(r.LeftTop(), sysPath.GetFullPath(), &maxwidth);
 	if (out != BRect(0, 0, -1, -1)) {
 		r = out;
 		r.OffsetBy(0, 10);
 	}
 
-	if (gPlatform == PLATFORM_HAIKU || gPlatform == PLATFORM_HAIKU_GCC4)
-	{
-		BView *commonheader = win->AddHeader(r.LeftTop(), TR("Common Libraries:"));
-		win->Lock();
-		r = commonheader->Frame();
-		win->Unlock();
+	if (gPlatform == PLATFORM_HAIKU || gPlatform == PLATFORM_HAIKU_GCC4) {
+		BView* commonHeader = window->AddHeader(r.LeftTop(),
+			TR("Common Libraries:"));
+		window->Lock();
+		r = commonHeader->Frame();
+		window->Unlock();
 		maxwidth = MAX(r.right, maxwidth);
 
-		r.OffsetBy(0, r.Height() + 10);
+		r.OffsetBy(0.0f, r.Height() + 10.0f);
 
-		out = win->ScanFolder(r.LeftTop(), GetSystemPath(B_USER_LIB_DIRECTORY).GetFullPath(),
-			&maxwidth);
-		if (out != BRect(0, 0, -1, -1))
-		{
+		out = window->ScanFolder(r.LeftTop(),
+			GetSystemPath(B_USER_LIB_DIRECTORY).GetFullPath(), &maxwidth);
+		if (out != BRect(0, 0, -1, -1)) {
 			r = out;
-			r.OffsetBy(0,10);
+			r.OffsetBy(0.0f, 10.0f);
 		}
 	}
 
-	BView *userheader = win->AddHeader(r.LeftTop(), TR("User Libraries:"));
-	win->Lock();
-	r = userheader->Frame();
-	win->Unlock();
-	maxwidth = MAX(r.right,maxwidth);
-	
+	BView* userHeader = window->AddHeader(r.LeftTop(), TR("User Libraries:"));
+	window->Lock();
+	r = userHeader->Frame();
+	window->Unlock();
+	maxwidth = MAX(r.right, maxwidth);
+
 	r.OffsetBy(0, r.Height() + 10);
-	
-	out = win->ScanFolder(r.LeftTop(), GetSystemPath(B_USER_LIB_DIRECTORY).GetFullPath(),
-		&maxwidth);
-	if (out.IsValid())
-	{
+
+	DPath userPath = GetSystemPath(B_USER_LIB_DIRECTORY);
+	out = window->ScanFolder(r.LeftTop(), userPath.GetFullPath(), &maxwidth);
+	if (out.IsValid()) {
 		r = out;
 		r.OffsetBy(0,10);
 	}
-	
-	win->Lock();
-	BView *top = win->GetBackgroundView();
-	BScrollView *scrollView = (BScrollView*)top->FindView("scrollView");
-	
-	BScrollBar *vbar = scrollView->ScrollBar(B_VERTICAL);
+
+	window->Lock();
+	BView* top = window->GetBackgroundView();
+	BScrollView* scrollView = (BScrollView*)top->FindView("scrollView");
+
+	BScrollBar* vbar = scrollView->ScrollBar(B_VERTICAL);
 	vbar->SetRange(0, r.bottom - scrollView->Bounds().Height());
 	vbar->SetSteps(r.Height() * 2.0,r.Height() * 8.0);
 	gSettings.Lock();
 	BRect savedframe;
-	if (gSettings.FindRect("libwin_frame",&savedframe) == B_OK)
-		win->ResizeTo(savedframe.Width(),savedframe.Height());
+	if (gSettings.FindRect("libwin_frame", &savedframe) == B_OK)
+		window->ResizeTo(savedframe.Width(), savedframe.Height());
 
 	gSettings.Unlock();
 
-	BStringView *label = (BStringView*)top->FindView("label");
+	BStringView* label = (BStringView*)top->FindView("label");
 	label->SetText(TR("Choose the system libraries for your project."));
 	float minw = label->Frame().right + 10;
-	win->SetSizeLimits(minw,30000,200,30000);
-	if (win->Bounds().Width() < minw)
-		win->ResizeTo(minw,win->Bounds().Height());
+	window->SetSizeLimits(minw, 30000, 200, 30000);
+	if (window->Bounds().Width() < minw)
+		window->ResizeTo(minw, window->Bounds().Height());
 
-	win->fScanThread = -1;
-	win->Unlock();
-	
+	window->fScanThread = -1;
+	window->Unlock();
+
 	return 0;
 }
-
--- Paladin/LibWindow.h
+++ Paladin/LibWindow.h
@@ -1,34 +1,53 @@
-#ifndef LIBWINDOW_H
-#define LIBWINDOW_H
+/*
+ * Copyright 2001-2010 DarkWyrm <bpmagic@columbus.rr.com>
+ * Copyright 2014 John Scipione <jscipione@gmail.com>
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		DarkWyrm, bpmagic@columbus.rr.com
+ *		John Scipione, jscipione@gmail.com
+ */
+#ifndef _LIB_WINDOW_H
+#define _LIB_WINDOW_H
+
 
 #include "DWindow.h"
 #include <Messenger.h>
 
-class Project;
 
 #define M_LIBWIN_CLOSED 'lwcl'
 
-class LibraryWindow : public DWindow
-{
+
+class Project;
+
+class LibraryWindow : public DWindow {
 public:
-			LibraryWindow(BRect frame, const BMessenger &parent,
-							Project *project);
-			~LibraryWindow(void);
-	bool	QuitRequested(void);
-	void	MessageReceived(BMessage *msg);
-	
+								LibraryWindow(BRect frame,
+									const BMessenger& parent,
+									Project* project);
+	virtual						~LibraryWindow(void);
+
+	virtual	void				MessageReceived(BMessage* message);
+	virtual	bool				QuitRequested(void);
+
 private:
-			BView *	AddHeader(BPoint location, const char *label);
-			BRect	ScanFolder(BPoint location, const char *dir, float *maxwidth);
-	static	int32	ScanThread(void *data);
-	
-	BView		*fCheckList;
-	
-	BMessenger	fParent;
-	Project		*fProject;
-	BList		fSystemLibs,
-				fUserLibs;
-	thread_id	fScanThread;
+			BView*				AddHeader(BPoint location,
+									const char* label);
+			BRect				ScanFolder(BPoint location,
+									const char* path,
+									float* maxWidth);
+	static	int32				ScanThread(void *data);
+
+			BView*				fCheckList;
+
+			BMessenger			fParent;
+			Project*			fProject;
+
+			BList				fSystemLibs;
+			BList				fUserLibs;
+
+			thread_id			fScanThread;
 };
 
-#endif
+
+#endif	// _LIB_WINDOW_H

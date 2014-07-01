3c0babebf83c39182aa42e6b88124234ce26c2a1
John Scipione <jscipione@gmail.com>
2014-06-30 20:09:27 -0400

    Convert VRegWindow to Layout APIs

--- Paladin/VRegWindow.cpp
+++ Paladin/VRegWindow.cpp
@@ -1,102 +1,88 @@
+/*
+ * Copyright 2001-2009 DarkWyrm <bpmagic@columbus.rr.com>
+ * Copyright 2014 John Scipione <jscipione@gmail.com>
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		DarkWyrm, bpmagic@columbus.rr.com
+ *		John Scipione, jscipione@gmail.com
+ */
+
+
 #include "VRegWindow.h"
 
-#include <Application.h>
 #include <Clipboard.h>
-#include <Font.h>
+#include <LayoutBuilder.h>
 #include <ScrollView.h>
 #include <StringView.h>
-#include <View.h>
+#include <TextControl.h>
+#include <TextView.h>
 
+#include "AutoTextControl.h"
 #include "CRegex.h"
 #include "PLocale.h"
 
-enum
-{
+
+enum {
 	M_REGEX_UPDATE = 'rxup'
 };
 
-VRegWindow::VRegWindow(void)
-	:	DWindow(BRect(100,100,500,400),TR("Regular Expression Tester"))
+
+VRegWindow::VRegWindow()
+	:
+	BWindow(BRect(0, 0, 640, 480), TR("Regular expression tester"),
+		B_TITLED_WINDOW, 0)
 {
-	BView *top = GetBackgroundView();
-	MakeCenteredOnShow(true);
-	
-	BRect r(Bounds().InsetByCopy(10,10));
-	fRegexBox = new AutoTextControl(r,"regex",TR("Regex:"),"", new BMessage(M_REGEX_UPDATE),
-									B_FOLLOW_LEFT_RIGHT | B_FOLLOW_TOP);
-	top->AddChild(fRegexBox);
-	float pwidth,pheight;
-	fRegexBox->GetPreferredSize(&pwidth, &pheight);
-	fRegexBox->ResizeTo(r.Width(),pheight);
-	r.bottom = r.top + pheight;
-	fRegexBox->SetDivider(be_plain_font->StringWidth(TR("Regex:")) + 5);
-	
-	r.OffsetBy(0,r.Height() + 10.0);
-	BStringView *sourceLabel = new BStringView(r,"sourcelabel",TR("Enter text to search here:"));
-	top->AddChild(sourceLabel);
-	sourceLabel->ResizeToPreferred();
-	
-	r.OffsetTo(10,sourceLabel->Frame().bottom + 5.0);
-	r.bottom = r.top + ((Bounds().bottom - r.top) / 2.0) - 5.0 - sourceLabel->Frame().Height();
-	r.right -= B_V_SCROLL_BAR_WIDTH;
-	
-	BRect textRect(r.InsetByCopy(10.0,10.0));
-	textRect.OffsetTo(10.0,10.0);
-	fSourceView = new BTextView(r,"sourcetext",textRect,B_FOLLOW_LEFT_RIGHT | B_FOLLOW_TOP);
-	
-	// I wonder why this flag isn't automatically set in the headers
-	fSourceView->SetFlags(fSourceView->Flags() | B_NAVIGABLE);
-	
-	BScrollView *sourceScroll = new BScrollView("scrollview",fSourceView,
-												B_FOLLOW_LEFT_RIGHT | B_FOLLOW_TOP,0,
-												false,true);
-	top->AddChild(sourceScroll);
-	
-	r.OffsetBy(0,r.Height() + 10.0);
-	BStringView *matchLabel = new BStringView(r,"matchlabel",TR("Matched text:"));
-	top->AddChild(matchLabel);
-	matchLabel->ResizeToPreferred();
-	
-	r.OffsetTo(10,matchLabel->Frame().bottom + 5.0);
-	fMatchView = new BTextView(r,"matchtext",textRect,B_FOLLOW_ALL);
-	BScrollView *matchScroll = new BScrollView("scrollview",fMatchView,
-												B_FOLLOW_ALL,0,false,true);
-	top->AddChild(matchScroll);
-	
+	fRegexTextControl = new AutoTextControl("regexTextControl", TR("Regex:"), "",
+		new BMessage(M_REGEX_UPDATE));
+
+	BStringView* sourceLabel = new BStringView("sourceLabel",
+		TR("Enter text to search here:"));
+	fSourceView = new BTextView("sourcetext", B_WILL_DRAW);
+	BScrollView* sourceScroll = new BScrollView("sourceScrollView", fSourceView,
+		B_WILL_DRAW, false, true);
+
+	BStringView* matchLabel = new BStringView("matchLabel",
+		TR("Matched text:"));
+	fMatchView = new BTextView("matchtext", B_WILL_DRAW);
+	BScrollView* matchScroll = new BScrollView("matchScrollView", fMatchView,
+		B_WILL_DRAW, false, true);
 	fSourceView->Paste(be_clipboard);
-	fRegexBox->MakeFocus(true);
+
+	BLayoutBuilder::Group<>(this, B_VERTICAL)
+		.AddGroup(B_HORIZONTAL)
+			.Add(fRegexTextControl->CreateLabelLayoutItem())
+			.Add(fRegexTextControl->CreateTextViewLayoutItem())
+			.End()
+		.AddGroup(B_VERTICAL, 0)
+			.Add(sourceLabel)
+			.Add(sourceScroll)
+			.End()
+		.AddGroup(B_VERTICAL, 0)
+			.Add(matchLabel)
+			.Add(matchScroll)
+			.End()
+		.SetInsets(B_USE_WINDOW_INSETS)
+		.End();
+
+	fRegexTextControl->MakeFocus(true);
+
+	CenterOnScreen();
 }
 
 
 void
-VRegWindow::MessageReceived(BMessage *msg)
+VRegWindow::MessageReceived(BMessage* message)
 {
-	switch (msg->what)
-	{
+	switch (message->what) {
 		case M_REGEX_UPDATE:
-		{
 			RunSearch();
 			break;
-		}
+
 		default:
-		{
-			BWindow::MessageReceived(msg);
+			BWindow::MessageReceived(message);
 			break;
-		}
 	}
-}
-
-
-void
-VRegWindow::FrameResized(float w, float h)
-{
-	BRect textRect(fSourceView->Bounds());
-	textRect.InsetBy(10.0,10.0);
-	fSourceView->SetTextRect(textRect);
-	
-	textRect = fMatchView->Bounds();
-	textRect.InsetBy(10.0,10.0);
-	fMatchView->SetTextRect(textRect);
 }
 
 
@@ -104,29 +90,32 @@ void
 VRegWindow::RunSearch(void)
 {
 	fMatchView->SetText("");
-	if (!fRegexBox->Text() || strlen(fRegexBox->Text()) < 1 || !fSourceView->Text() ||
-		strlen(fSourceView->Text()) < 1)
+	if (fRegexTextControl->Text() == 0 || *fRegexTextControl->Text() == '\0'
+		|| fSourceView->Text() == NULL || *fSourceView->Text() == '\0') {
 		return;
+	}
 
-	CRegex re(fRegexBox->Text(),false,false,false);
-	
-	int32 startpos = 0;
-	status_t err = re.Match(fSourceView->Text(),strlen(fSourceView->Text()),startpos,
-							PCRE_NO_UTF8_CHECK);
-	
+	CRegex regex(fRegexTextControl->Text(), false, false, false);
+	int32 startingPosition = 0;
+	status_t result = regex.Match(fSourceView->Text(), strlen(fSourceView->Text()),
+		startingPosition, PCRE_NO_UTF8_CHECK);
+
 	int32 resultCount = 0;
-	while (err == B_OK)
-	{
-		if (re.MatchCount())
-			startpos = re.MatchStart(re.MatchCount() - 1) + re.MatchLen(re.MatchCount() - 1) + 1;
-		
+	while (result == B_OK) {
+		if (regex.MatchCount() > 0) {
+			startingPosition = regex.MatchStart(regex.MatchCount() - 1)
+				+ regex.MatchLen(regex.MatchCount() - 1) + 1;
+		}
+
 		BString match;
-		for (int32 i = 0; i < re.MatchCount(); i++)
-			match << resultCount << ":\t" << re.MatchStr(fSourceView->Text(),i);
+		for (int32 i = 0; i < regex.MatchCount(); i++)
+			match << resultCount << ":\t" << regex.MatchStr(fSourceView->Text(), i);
+
 		match << "\n";
 		fMatchView->Insert(match.String());
-		err = re.Match(fSourceView->Text(),strlen(fSourceView->Text()),startpos, PCRE_NO_UTF8_CHECK);
+		result = regex.Match(fSourceView->Text(), strlen(fSourceView->Text()),
+			startingPosition, PCRE_NO_UTF8_CHECK);
+
 		resultCount++;
 	}
 }
-
--- Paladin/VRegWindow.h
+++ Paladin/VRegWindow.h
@@ -1,25 +1,35 @@
-#ifndef VREGWINDOW_H
-#define VREGWINDOW_H
+/*
+ * Copyright 2001-2009 DarkWyrm <bpmagic@columbus.rr.com>
+ * Copyright 2014 John Scipione <jscipione@gmail.com>
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		DarkWyrm, bpmagic@columbus.rr.com
+ *		John Scipione, jscipione@gmail.com
+ */
+#ifndef _V_REG_WINDOW_H
+#define _V_REG_WINDOW_H
 
-#include "DWindow.h"
-#include <MenuBar.h>
-#include <TextView.h>
 
-#include "AutoTextControl.h"
+#include <Window.h>
 
-class VRegWindow : public DWindow
-{
+
+class AutoTextControl;
+class BTextView;
+
+class VRegWindow : public BWindow {
 public:
-						VRegWindow(void);
-			void		MessageReceived(BMessage *msg);
-			void		FrameResized(float w, float h);
+								VRegWindow();
+
+	virtual	void				MessageReceived(BMessage* message);
 			
 private:
-			void		RunSearch(void);
-	AutoTextControl		*fRegexBox;
-	BTextView			*fSourceView,
-						*fMatchView;
+			void				RunSearch(void);
+
+			AutoTextControl*	fRegexTextControl;
+			BTextView*			fSourceView;
+			BTextView*			fMatchView;
 };
 
-#endif
 
+#endif // _V_REG_WINDOW_H

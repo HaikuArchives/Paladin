252021d162878a0e5b46f8831301cd28dceb1a91
Augustin Cavalier <waddlesplash@gmail.com>
2014-12-23 23:13:12 -0500

    Convert RunArgsWindow to the layout kit.

--- Paladin/RunArgsWindow.cpp
+++ Paladin/RunArgsWindow.cpp
@@ -9,32 +9,31 @@
 #include "PLocale.h"
 #include "Project.h"
 
+#include <LayoutBuilder.h>
+
 #define	M_FIND_FILE 'fnfl'
 
 
 RunArgsWindow::RunArgsWindow(Project *proj)
-	:	DWindow(BRect(0,0,400,300),"Run arguments",B_TITLED_WINDOW,
+	:	DWindow(BRect(0,0,400,35),"Run arguments",B_TITLED_WINDOW,
 				B_ASYNCHRONOUS_CONTROLS | B_NOT_RESIZABLE),
 		fProject(proj)
 {
 	AddCommonFilter(new EscapeCancelFilter());
 	
-	BView *top = GetBackgroundView();
-	
-	fArgText = new AutoTextControl(BRect(10,10,11,11),"argtext",TR("Arguments: "),
-									fProject->GetRunArgs(), new BMessage,
-									B_FOLLOW_LEFT_RIGHT | B_FOLLOW_TOP);
-	top->AddChild(fArgText);
-	fArgText->ResizeToPreferred();
-	fArgText->ResizeTo(Bounds().Width() - 20,fArgText->Bounds().Height());
+	fArgText = new AutoTextControl("argtext",TR("Arguments: "),
+									fProject->GetRunArgs(), new BMessage);
 	fArgText->SetDivider(fArgText->StringWidth(TR("Arguments: ")) + 5);
-	
-	ResizeTo(Bounds().Width(),fArgText->Frame().bottom + 10);
 	
 	MakeCenteredOnShow(true);
 	
 	fArgText->MakeFocus(true);
 	fArgText->TextView()->SelectAll();
+	
+	BLayoutBuilder::Group<>(this, B_HORIZONTAL)
+		.SetInsets(5)
+		.Add(fArgText)
+	.End();
 }
 
 

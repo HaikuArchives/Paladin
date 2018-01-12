2f2fc858a9536adcc3ef36455c84f1c29c20a98d
Bach Nguyen <bach5000@gmail.com>
2018-01-12 12:58:28 -0600

    Update makefile template

--- Paladin/Makefile.h
+++ Paladin/Makefile.h
@@ -12,11 +12,13 @@
  * 5. Paste it into here.
  */
 
-const char* template_makefile = "## Haiku Generic Makefile ##\n\
+const char* template_makefile = "## Haiku Generic Makefile v2.6 ##\n\
 \n\
 ## Fill in this file to specify the project being created, and the referenced\n\
 ## Makefile-Engine will do all of the hard work for you. This handles any\n\
 ## architecture of Haiku.\n\
+## For more information, see:\n\
+## file://system/develop/documentation/makefile-engine.html\n\
 \n\
 # The name of the binary.\n\
 NAME = $@NAME@$\n\

da4719fd813e7ebd275632fbff91f8e79e53a361
Adam Fowler <adamfowleruk@gmail.com>
2019-12-28 15:13:41 +0000

    Fixed #322. Added latest catkeys and tested build

--- Tests/compile.sh
+++ Tests/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "Assembling Paladin as a static library (You MUST compile it successfully first)"
-cd ../Paladin/objects.x86_64-cc8-release
+cd ../Paladin/objects*
 ar rvs paladin.a *.o
 echo "Compiling and linking tests against Paladin static library"
 cd ../../Tests
-g++ Main.cpp ProjectTests.cpp CompileCommandsJSONTests.cpp ../Paladin/objects.x86_64-cc8-release/paladin.a -o ./tests.o -Wall -lUnitTest++ -I../Paladin -I../Paladin/SourceControl -I../Paladin/BuildSystem -I../Paladin/ThirdParty -I../Paladin/PreviewFeatures -fprofile-arcs -ftest-coverage -lbe -lstdc++ -llocalestub
+g++ Main.cpp ProjectTests.cpp CompileCommandsJSONTests.cpp ../Paladin/objects*/paladin.a -o ./tests.o -Wall -lUnitTest++ -I../Paladin -I../Paladin/SourceControl -I../Paladin/BuildSystem -I../Paladin/ThirdParty -I../Paladin/PreviewFeatures -fprofile-arcs -ftest-coverage -lbe -lstdc++ -llocalestub
 echo "Done. Now execute ./tests.o"
 

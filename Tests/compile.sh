402d53a4258d5b44269fbdfe4935700b249a8649
Michael Brumbelow <mbrumbelow@mac.com>
2023-04-15 16:18:09 -0400

    Fixed a pathing problem with the linking of the Paladin as a static library for use in the test suite. Cleaned/formatted items in the compile script.

--- Tests/compile.sh
+++ Tests/compile.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-echo "Assembling Paladin as a static library (You MUST compile it successfully first)"
-cd ../Paladin/objects*
+
+echo "Assembling Paladin as a static library"
+cd ../Paladin/objects.*-release
 ar rvs paladin.a *.o
+
 echo "Compiling and linking tests against Paladin static library"
 cd ../../Tests
-g++ 	Main.cpp \
+g++ Main.cpp \
 	ProjectTests.cpp \
 	CompileCommandsJSONTests.cpp \
 	CommandOutputHandlerTests.cpp \
 	../Paladin/objects*/paladin.a -o ./tests.o -Wall -lUnitTest++ -I../Paladin -I../Paladin/SourceControl -I../Paladin/BuildSystem -I../Paladin/ThirdParty -I../Paladin/PreviewFeatures -fprofile-arcs -ftest-coverage -lgcov -lbe -llocalestub
+
 echo "Done. Now execute ./tests.o"
 

83b3d351919a86d7834084e02ccc6b7b9db21ffd
Jerome Duval <jerome.duval@gmail.com>
2024-03-16 17:48:46 +0100

    fix tests on gcc2

--- Tests/compile.sh
+++ Tests/compile.sh
@@ -4,13 +4,18 @@ echo "Assembling Paladin as a static library"
 cd ../Paladin/objects.*-release
 ar rvs paladin.a *.o
 
+LIBGCOV=-lgcov
+if [[ `uname -m` == BePC ]]; then
+  LIBGCOV=
+fi
+
 echo "Compiling and linking tests against Paladin static library"
 cd ../../Tests
 g++ Main.cpp \
 	ProjectTests.cpp \
 	CompileCommandsJSONTests.cpp \
 	CommandOutputHandlerTests.cpp \
-	../Paladin/objects*/paladin.a -o ./tests.o -Wall -lUnitTest++ -I../Paladin -I../Paladin/SourceControl -I../Paladin/BuildSystem -I../Paladin/ThirdParty -I../Paladin/PreviewFeatures -fprofile-arcs -ftest-coverage -lgcov -lbe -llocalestub
+	../Paladin/objects*/paladin.a -o ./tests.o -Wall -lUnitTest++ -I../Paladin -I../Paladin/SourceControl -I../Paladin/BuildSystem -I../Paladin/ThirdParty -I../Paladin/PreviewFeatures -fprofile-arcs -ftest-coverage $LIBGCOV -lbe -llocalestub
 
 echo "Done. Now execute ./tests.o"
 
